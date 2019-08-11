3a6f24d17b190727bcf10fa5d83b21273fa51995
Kevin Berridge <kevin.w.berridge@gmail.com>
2019-08-11 19:14:59 -0400

    prototype UI

--- src/PlayMasterMind.ml
+++ src/PlayMasterMind.ml
@@ -3,32 +3,140 @@ open Tea.Html
 open MasterMind
 
 type msg =
-  | SetPeg of int * codePeg
+  | SetPeg of int * string
   | Guess
+
+type guess = {
+  one: string;
+  two: string;
+  three: string;
+  four: string;
+}
+
+type pastGuess = {
+  guess: codePeg list;
+  score: score
+}
 
 type gameState = {
   secret: codePeg list;
-  guesses: codePeg list list
+  currentGuess: guess;
+  pastGuesses: pastGuess list
 }
+
+let pegToLetter = function
+  | Red -> "R"
+  | Green -> "G"
+  | Blue -> "B"
+  | Yellow -> "Y"
+  | Orange -> "O"
+  | Purple -> "P"
+
+exception InvalidLetter
+
+let letterToPeg = function
+  | "R" -> Red
+  | "G" -> Green
+  | "B" -> Blue
+  | "Y" -> Yellow
+  | "O" -> Orange
+  | "P" -> Purple
+  | _ -> raise InvalidLetter
+  
+let emptyGuess = {one=""; two=""; three=""; four=""}
 
 let init () = {
-  secret = [Red; Red; Green; Green]; (* generate! *)
-  guesses = []
+  secret = [Red; Red; Green; Green]; (* todo: generate! *)
+  currentGuess = emptyGuess;
+  pastGuesses = []
 }
 
+let setPeg currentGuess index peg_str = 
+  match index with
+  | 0 -> { currentGuess with one = peg_str }
+  | 1 -> { currentGuess with two = peg_str }
+  | 2 -> { currentGuess with three = peg_str }
+  | 3 -> { currentGuess with four = peg_str }
+  | _ -> raise InvalidLetter
+
+let handleGuess model =
+  let guess = [
+    (letterToPeg model.currentGuess.one);
+    (letterToPeg model.currentGuess.two);
+    (letterToPeg model.currentGuess.three);
+    (letterToPeg model.currentGuess.four)
+  ] 
+  in let score = getScore guess model.secret
+  in { model with 
+    currentGuess = emptyGuess; 
+    pastGuesses = { guess; score } :: model.pastGuesses
+  }
+
 let update model = function
-  | SetPeg (index, pegColor) -> model
-  | Guess -> model
+  | SetPeg (index, peg_str) -> 
+    { model with 
+      currentGuess = (setPeg model.currentGuess index peg_str)
+    }
+  | Guess -> handleGuess model
+
+let view_peginput peg_str n =
+  input'
+    [ value peg_str
+    ; onInput (fun str -> (SetPeg (n, str)))
+    ; Vdom.attribute "" "maxlength" "1"
+    ]
+    []
+
+let view_enterguess model = 
+  div
+    []
+    [ view_peginput model.currentGuess.one 0
+    ; view_peginput model.currentGuess.two 1
+    ; view_peginput model.currentGuess.three 2
+    ; view_peginput model.currentGuess.four 3
+    ; button
+      [ onClick (Guess) ]
+      [ text "Guess" ]
+    ]
+
+let view_pastguess pastGuess =
+  let guess = pastGuess.guess
+  in div
+    []
+    [ text (pegToLetter (List.nth guess 0));
+      text (pegToLetter (List.nth guess 1));
+      text (pegToLetter (List.nth guess 2));
+      text (pegToLetter (List.nth guess 3));
+      text "SCORE: ";
+      text ("exact: " ^ (string_of_int pastGuess.score.exactMatches));
+      text ("partial: " ^ (string_of_int pastGuess.score.colorMatches))
+    ]
+
+let view_pastguesses model =
+  div
+    []
+    (List.map view_pastguess model.pastGuesses)
 
 let view model =
   div
     []
-    [ text "Welcome to MasterMind!"
+    [ header
+      [ style "text-align" "center"]
+      [ h1 [] [ text "MasterMind!" ] ]
+    ; view_enterguess model
+    ; div
+      []
+      [ text model.currentGuess.one
+      ; text model.currentGuess.two
+      ; text model.currentGuess.three
+      ; text model.currentGuess.four
+      ]
+    ; view_pastguesses model
     ]
 
 let main =
-  beginnerProgram { (* The beginnerProgram just takes a set model state and the update and view functions *)
-    model = init (); (* Since model is a set value here, we call our init function to generate that value *)
+  beginnerProgram {
+    model = init ();
     update;
     view;
   }
